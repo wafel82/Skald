da1e962bf683a8860c274dcacffa4d8e8603249f
wafel82 <wojtek.wawerek@estimote.com>
2018-02-20 23:28:33 +0100

    Add some java docs

--- android-skald/src/main/java/com/wafel/skald/api/ConfigApi.kt
+++ android-skald/src/main/java/com/wafel/skald/api/ConfigApi.kt
@@ -3,29 +3,75 @@ package com.wafel.skald.api
 import com.wafel.skald.internals.config.SimpleSkald
 import com.wafel.skald.internals.config.ScaldAppender
 
-
+/**
+ * Logging levels to be used when configuring skald
+ */
 enum class LogLevel { NONE, WTF, ERROR, WARN, INFO, DEBUG, TRACE }
 
+
+/**
+ * Main function you begin Skald configuration with
+ * @param init - function type with Skald instance as a receiver. Inside this function you can
+ * refer to all Skald's public method without 'this.' prefix. Use this function to add sagas
+ * to Skald configuration.
+ * @return - configured skald instance
+ */
 fun skald(init: Skald.()->Unit): Skald {
     val scaldConfiguration = SimpleSkald()
     scaldConfiguration.init()
     return scaldConfiguration
 }
 
+/**
+ * Main framework class holding its configuration
+ */
 interface Skald {
+    /**
+     * Method to be used to add new Saga configuration.
+     * @param init - function type with Saga instance as a receiver. Inside this function you can
+     * refer to all Saga's public method without 'this.' prefix. Use this function to configure
+     * newly added saga instance.
+     */
     fun writeSaga(init: Saga.()->Unit)
 }
 
+/**
+ * Class defining saga configuration capabilities
+ */
 abstract class Saga {
+    /**
+     * Function adds Logcat appender to this Saga instance.
+     * @param init - function type with LogcatAppender instance as a receiver. Inside this function
+     * you can refer to all LogcatAppender's public method without 'this.' prefix.
+     */
     abstract fun toLogcat(init: LogcatAppender.() -> Unit)
+
+    /**
+     * Function configures log level to be used with this Saga instance.
+     * @param level - lambda returning one of the @LogLevel enum instance
+     */
     abstract fun withLevel(level: ()-> LogLevel)
+
+    /**
+     * Function configures path to be used with this Saga instance. Provided path will be compared
+     * with the one defined when creating logger (@see createLogger methods). When logger path starts
+     * with Saga path - then saga will be used by this logger.
+     *@param path - lambda returning path to be configured.
+     */
     abstract fun withPath(path: ()->String)
     internal abstract fun getLevel(): LogLevel
     internal abstract fun getPath(): String
     internal abstract fun getAppenders(): List<ScaldAppender>
 }
 
+/**
+ * Logcat appender configuration
+ */
 interface LogcatAppender {
+    /**
+     * Function configures tag to be used when logging to logcat.
+     * @param tag - lambda returning tag string to be used.
+     */
     fun withTag(tag: ()->String)
 }
 
--- android-skald/src/main/java/com/wafel/skald/api/LoggerApi.kt
+++ android-skald/src/main/java/com/wafel/skald/api/LoggerApi.kt
@@ -2,11 +2,25 @@ package com.wafel.skald.api
 
 import com.wafel.skald.internals.logger.SimpleSkaldLogger
 
-
+/**
+ * Logger factory method
+ * @param path - path to be used by constructed logger. It will be compared with configured
+ * Sagas' paths. When logger path starts with Saga path then such Saga will be used to log messages
+ * with this logger.
+ */
 fun createLogger(path: String): SkaldLogger = SimpleSkaldLogger(path)
 
+/**
+* Logger factory method
+* @param path - class to be used to construct logger path - class canonical name will be used.
+* Constructed path will be compared with configured Sagas' paths. When logger path starts with
+* Saga path then such Saga will be used to log messages with this logger.
+*/
 fun createLogger(clazz: Class<*>): SkaldLogger = SimpleSkaldLogger(clazz.canonicalName)
 
+/**
+ * Skald logger interface. Use its approproate methods in logging entry  points
+ */
 interface SkaldLogger {
     fun wtf(message: String)
     fun error(message: String)
