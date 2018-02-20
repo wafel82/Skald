25a12d5f424faf8a177b5176a6355ed59813f853
wafel82 <wojtek.wawerek@estimote.com>
2018-02-20 22:06:50 +0100

    Logcat appender - Draft

---
+++ android-skald/src/main/java/com/wafel/skald/internals/config/SimpleLogcatAppender.kt
@@ -0,0 +1,35 @@
+package com.wafel.skald.internals.config
+
+import android.util.Log
+import com.wafel.skald.api.LogcatAppender
+
+internal class SimpleLogcatAppender : LogcatAppender, ScaldAppender {
+    private var tag = "SCALD"
+    override fun withTag(tag: ()->String) {
+        this.tag = tag()
+    }
+
+    override fun wtf(message: String) {
+        Log.wtf(tag, message)
+    }
+
+    override fun error(message: String) {
+        Log.e(tag, message)
+    }
+
+    override fun warn(message: String) {
+        Log.w(tag, message)
+    }
+
+    override fun info(message: String) {
+        Log.i(tag, message)
+    }
+
+    override fun debug(message: String) {
+        Log.d(tag, message)
+    }
+
+    override fun trace(message: String) {
+        Log.v(tag, message)
+    }
+}
